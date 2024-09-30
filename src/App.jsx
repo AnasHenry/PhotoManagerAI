10275404ca3f77caa042b2ea6a440eee8239bbf8
Anas <anashenrya@gmail.com>
2024-09-30 10:01:10 +0530

    Added Login Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
-import { useState } from 'react'
-function App() {
-  const [count, setCount] = useState(0)
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import UserCon from './components/UserCon';
+import ClientRegister from './components/ClientRegister';
+import Register from './components/register';
+import Login from './components/Login';
 
+function App() {
   return (
-    <div>
-      <h1>Hi</h1>
-    </div>
-  )
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/userselect" element={<UserCon />} />
+          <Route path="/register-client" element={<ClientRegister />} />
+          <Route path="/register" element={<Register />} />
+          <Route path='/login' element={<Login/>}/>
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
-export default App
+export default App;
